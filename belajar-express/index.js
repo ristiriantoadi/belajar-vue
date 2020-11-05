8d147b174c64bafdbc0997587398ecf921bb953a
Ristirianto Adi <ristiriantoadi@gmail.com>
2020-11-05 15:05:58 +0800

    mongodb api done

--- belajar-express/index.js
+++ belajar-express/index.js
@@ -1,46 +1,42 @@
 const express = require('express')
-// var mongo = require('mongodb');
-
-const app = express()
-
-const PORT = process.env.port || 5000
-
-// var MongoClient = require('mongodb').MongoClient;
-// var url = "mongodb://localhost:27017/mydb";
-
-// MongoClient.connect(url, function(err, db) {
-//   if (err) throw err;
-//   console.log("Database created!");
-//   db.close();
-// });
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser')
 
 //import routes
 const postsRoute = require('./routes/posts')
 
 
+const app = express()
+const PORT = process.env.port || 5000
+
+
+app.use(bodyParser.json())
+
+
+//routing
 app.use('/posts',postsRoute)
-
 app.use(express.static('./public'))
-
 app.get('/', function (req, res) {
     res.send('Hello World!')
   })
-
 app.post('/', function (req, res) {
     res.send('Got a POST request')
 })
-
 app.put('/user', function (req, res) {
     res.send('Got a PUT request at /user')
 })
-
 app.delete('/user', function (req, res) {
     res.send('Got a DELETE request at /user')
 })
 
-const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://ristiriantoadi:globalwarming@cluster0.dn2im.mongodb.net/contoh?retryWrites=true&w=majority', {useNewUrlParser: true,useUnifiedTopology:true},()=>{
-    console.log("connected to db")
-});
+//connect to mongodb
+mongoose.connect('mongodb+srv://ristiriantoadi:globalwarming@cluster0.dn2im.mongodb.net/contoh?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true});
+const connection = mongoose.connection;
+connection.once("open", function() {
+    console.log("MongoDB database connection established successfully");
+  });
+  
+
+
 
 app.listen(PORT,()=>{console.log(`Server listen at port ${PORT}`)})
--- belajar-express/routes/posts.js
+++ belajar-express/routes/posts.js
@@ -1,13 +1,69 @@
 const express = require('express')
-
 const router = express.Router()
+const Post = require('../models/Post')
 
-router.get('/',(req,res)=>{
-    res.send("We are on posts")
+
+
+
+router.get('/', async (req,res)=>{
+    try{
+        const posts = await Post.find()
+        res.json(posts)
+    }catch(err){
+        res.json({message:err})
+    }
 })
 
-router.get('/specific',(req,res)=>{
-    res.send("we are on specific post")
+router.get('/:postId',async (req,res)=>{
+    // res.send("we are on specific post")
+    console.log(req.params.postId)
+    try{
+        const post = await Post.findById(req.params.postId)
+        res.json(post)
+    }catch(err){
+        res.json({message:err})
+    }
+})
+
+router.delete('/:postId',async (req,res)=>{
+    // res.send("we are on specific post")
+    console.log(req.params.postId)
+    try{
+        const removedPost = await Post.remove({_id:req.params.postId})
+        res.json(removedPost)
+    }catch(err){
+        res.json({message:err})
+    }
+})
+
+router.patch('/:postId',async (req,res)=>{
+    console.log(req.params.postId)
+    try{
+        // const removedPost = await Post.remove({_id:req.params.postId})
+        // res.json(removedPost)
+        const updatedPost = await Post.updateOne(
+            {_id:req.params.postId},
+            {$set:{title:req.body.title}})
+        res.json(updatedPost)
+    }catch(err){
+        res.json({message:err})
+    }
+})
+
+router.post('/',async (req,res)=>{
+    // console.log(req.body)
+    const post = new Post({
+        title:req.body.title,
+        description:req.body.description
+    })
+    console.log(req.body)
+    try{
+        const savedPost = await post.save()
+        res.json(savedPost)
+    }catch(error){
+        // console.log(error)
+        res.json({message:error})
+    }
 })
 
 module.exports=router
